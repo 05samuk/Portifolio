c62ecd47e1e8139914d04b40b919fcaaca880284
05samuk <samuelribs1308@gmail.com>
2024-06-14 21:25:02 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# SAMUEl
+
+O projeto foi desenvolvido em html e css e javascript usando todas as regras tanto semantico, foi interessante pois eu aprendi muito com o projeto 
+
+# instalação
+
+``` git clone https://github.com/05samuk/Portifolio ```
---
+++ js/java.js
@@ -0,0 +1,40 @@
+function GitAbrir(){
+    window.open("https://github.com/05samuk", '_blank');
+ }
+ 
+ function gmailAbrir(){
+     window.open("https://mail.google.com/mail/u/0/?tab=rm&https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=GTvVlcSKjssSBhtZJjKqRkwMdGFGKqbkjsLPlQDhfzTbtCBVVSBzhxbnLgXTBWpKcGjNRNBNHxqCSogbl#inbox?compose=new", '_blank');
+ }
+ 
+ function linkedinAbrir(){
+     window.open("https://www.linkedin.com/in/samuel-ribeiro-22b635289/", '_blank');
+ }
+
+ function googleColabAbrir(){
+    window.open("https://colab.research.google.com/drive/1jk_dqpIjkvmcH4uFEBSbxIgWX5s7QOCd#scrollTo=wypIF_SPN8Eq", '_blank');
+}
+//colocar link do colab
+ 
+ function voltarHome (){
+     window.location.href = ('../index.html')
+ }
+ 
+ function web(){
+     window.location.href = ('../pages/web.html');
+ }
+ 
+ function BD(){
+     window.location.href = ('../pages/bd.html');
+ }
+ 
+ function Cd(){
+     window.location.href = ('../pages/cd.html');
+ }
+ 
+ function Aind(){
+     window.location.href = ('../pages/a_ind.html');
+ }
+ 
+ function LP(){
+     window.location.href = ('../pages/lp.html');
+ }
